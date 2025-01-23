088f2905d1a7f692ee111596e930e1b10b37b689
Jibin T J <jibintj6351@gmail.com>
2025-01-23 12:45:51 +0530

    created header and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { BookingComponent } from './booking/booking.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    BookingComponent,
+    SignInComponent,
+    SignUpComponent
+
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BookingComponent } from './booking/booking.component';
+import { AboutComponent } from './about/about.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'booking', component: BookingComponent },
+  { path: 'about', component: AboutComponent },
+  {path:'sign-in', component:SignInComponent},
+  {path:'sign-up',component:SignUpComponent}
+];
