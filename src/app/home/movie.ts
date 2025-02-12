c88c9ca5e4850bd2e37a01b8af2a0723bd6d8248
Jibin <jibintj6351@gmail.com>
2025-02-12 16:14:45 +0530

    completed like button

--- src/app/home/movie.ts
+++ src/app/home/movie.ts
@@ -1,12 +1,12 @@
 
 export interface Movie {
-    id: number;
-    rating: string;
-    releaseDate: string;
-    description: string;
-    director: string;
-    title: string;
-    genre: string;
-    path: string;
-    views:number;
-  }
+  id: number;
+  rating: string;
+  releaseDate: string;
+  description: string;
+  director: string;
+  title: string;
+  genre: string;
+  path: string;
+  views: number;
+}
